9055f2ec23b39fae83933a48286c1b7a8d340b7e
rs8242813 <rs8242813@gmail.com>
2025-06-16 19:38:39 +0530

    Add files via upload

--- Task2.py
+++ Task2.py
@@ -1,14 +1,7 @@
-# Taking user input for first and last name
-first_name = input("Enter your first name: ")
-last_name = input("Enter your last name: ")
+# Taking name as input string
+first = input("Enter first name: ")
+last = input("Enter last name: ")
+fullname = f"{first} {last}"
 
-# Function to generate a friendly greeting
-def greet_user(first_name, last_name):
-    # Formatting the full name properly
-    full_name = f"{first_name} { last_name}"
-
-    # Printing the personalized greeting message
-    print(f"\n Hello, {full_name}! Hope you're having an amazing day!")
-
-# Calling the function to display the greeting
-greet_user(first_name, last_name)
+#Greeting
+print(f"Hello, {fullname}! Nice to meet you.")
